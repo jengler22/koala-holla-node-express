65809ad9a05fddee46ae1d4121ec55fedc69c22f
Glonel Dimapilis <gdimapilis@aol.com>
2023-03-02 19:20:40 -0600

    finished base mode functionality for the week

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,13 +3,76 @@ console.log( 'js' );
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
+  axios.get('/koalas').then((response) => {
+    console.log(response);
+    let koalasFromServer = response.data;
+    let contentDiv = document.querySelector("#viewKoalas");
+    contentDiv.innerHTML = '';
+    let i = 0;
+    for(let koala of koalasFromServer) {
+      contentDiv.innerHTML += `
+      <tr>
+          <td>${koala.name}</td>
+          <td>${koala.age}</td>
+          <td>${koala.gender}</td>
+          <td>${koala.readyForTransfer}</td>
+          <td>${koala.notes}</td>
+          <td><button id="deleteButton" onclick="deleteKoala(${i})">Delete</button></td>
+      </tr>
+      `;
+      i += 1;
+    }
+  }).catch((error)=> {
+    console.log(error);
+    alert('koala ecaped! also, something is wrong.');
+  });
   
 } // end getKoalas
 
+//TODO possible database questin??????????? 
+/*
 function saveKoala(){
   console.log( 'in saveKoala' );
   // axios call to server to get koalas
  
 }
+*/
 
 getKoalas();
+
+//* addKoala() function. Taking all inputs and storing into object
+function addKoala(event) {
+  let koalaName = document.querySelector('#nameIn').value;
+  let koalaAge = document.querySelector('#ageIn').value;
+  let koalaGender = document.querySelector('#genderIn').value;
+  let koalaTransfer = document.querySelector('#readyForTransferIn').value;
+  let koalaNotes = document.querySelector('#notesIn').value;
+  
+  let koalaForServer = {
+      name: koalaName,
+      age: koalaAge,
+      gender: koalaGender,
+      readyForTransfer: koalaTransfer,
+      notes: koalaNotes
+  };
+
+  axios.post( '/koalas', koalaForServer ).then((response) => {
+      console.log( response );
+      getKoalas();  
+  }).catch((error) => {
+      console.log( error );
+      alert( 'Something went wrong.' );
+  })
+
+};
+
+function deleteKoala(index) {
+    console.log(`Deleting koala ${index}`);
+    axios.delete(`/koalas/${index}`).then((response) => {
+      console.log(response);
+      getKoalas();
+    }).catch((error) =>{
+      console.log(error);
+      alert('Whoops. Is that even a koala?!');
+    })
+};
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+let koalaList = [];
+
 // DB CONNECTION
 
 
 // GET
-
+koalaRouter.get( '/', (req, res) => {
+    console.log( 'GET Request made for /koalas' );
+    res.send( koalaList );
+});
 
 // POST
-
+koalaRouter.post( '/', (req, res) => {
+    console.log( 'POST request made for /koalas' );
+    console.log( req.body );
+    let koalaToAdd = req.body;
+    koalaList.push( koalaToAdd );
+    res.sendStatus(201);
+});
 
 // PUT
 
 
 // DELETE
+koalaRouter.delete( '/:id', (req, res) => {
+    console.log( req.params.id );
+    const deleteIndex = Number( req.params.id );
+    koalaList = koalaList.filter( (koala, index) => index !== deleteIndex )
+    res.sendStatus(200);
+});
+
+
+
 
 module.exports = koalaRouter;
